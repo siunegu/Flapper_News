0c52f38a97da017e55fb2d5d8d1157b78eef9b2c
Eugenius Hei Man Lai <equalsbrackets@gmail.com>
2015-03-18 11:25:13 +1100

    added CDN Bootstrap 3.3.4 and bootstrap classes to index.html

--- app.js
+++ app.js
@@ -14,9 +14,14 @@
 	  ]
 
 	  $scope.addPost = function(){
-	  	// if(!$scope.title || $scope.title === ''){return;}
-	  	$scope.posts.push({title: $scope.title, upvotes: 0}) 
+	  	if(!$scope.title || $scope.title === ''){return;}
+	  	$scope.posts.push({
+	  		title: $scope.title, 
+	  		link: $scope.link,
+	  		upvotes: 0
+	  	}) 
 	  	$scope.title = '';
+	  	$scope.link = '';
 	  };
 
 	  $scope.incrementUpvotes = function(post){
