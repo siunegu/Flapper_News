4a2a051dc5b3ee9e13b4da573afa93f190cb4e36
Eugenius Hei Man Lai <equalsbrackets@gmail.com>
2015-03-18 15:47:15 +1100

    refactored the posts variable into a service, call service in controller

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 (function() {
 
-	var app = angular.module('flapperNews', []);
+	var app = angular.module('flapperNews', [])
+	.factory('posts', [function(){
+		var o = {
+			posts: [] 
+		}
+		return o;
+	}]);
+
 	app.controller('MainCtrl', [
 	'$scope',
-	function($scope){
+	'posts',
+	function($scope, posts){
 
-	  $scope.posts = [
-	  	{title: 'post 1', upvotes: 5},
-	  	{title: 'post 2', upvotes: 2},
-	  	{title: 'post 3', upvotes: 15},
-	  	{title: 'post 4', upvotes: 9},
-	  	{title: 'post 5', upvotes: 4}	  	
-	  ]
+	  $scope.posts = posts.posts;
 
 	  $scope.addPost = function(){
 	  	if(!$scope.title || $scope.title === ''){return;}
